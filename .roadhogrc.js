e7a0d8bd6319186e28fc5e4ec7494f1635d4549b
Xingyu Chen <chenxingyu.cs@gmail.com>
2017-08-12 16:34:09 -0700

    Build the skeleton

---
+++ .roadhogrc.js
@@ -0,0 +1,29 @@
+
+export default {
+  entry: 'src/index.js',
+  env: {
+    development: {
+      extraBabelPlugins: [
+        'dva-hmr',
+        'transform-runtime',
+        [
+          "import", {
+            "libraryName": "antd",
+            "style": true
+          }
+        ]
+      ],
+    },
+    production: {
+      extraBabelPlugins: [
+        'transform-runtime',
+        [
+          "import", {
+            "libraryName": "antd",
+            "style": true
+          }
+        ]
+      ],
+    },
+  },
+}
